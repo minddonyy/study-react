1e1fa0c123870e9ed966de4e1af9459c0194c0b3
minddonyy <minddonyy@gmail.com>
2021-04-12 17:44:57 +0900

    [컴포넌트스타일링] Sass

---
+++ styling-react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import SassComponent from "./sassComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
+}
+export default App;
